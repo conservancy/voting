89126c6b38719f6ed3cf9f0012513cd22697de2c
Bradley M. Kuhn <bkuhn@ebb.org>
2014-02-10 17:41:31 -0500

    Improve the text for Step 2/4.
    I'm trying to get a version of this text that is clear.  This is another
    attempt.

--- vote/include/step2-choose.php
+++ vote/include/step2-choose.php
@@ -8,13 +8,13 @@ function step2_do () {
 
   $result = "<h2>Step 2/4 - Choose your vote</h2>\n";
 
-  $result .= "<p>Clicking on a candidate in the Candidates box adds it in order to the Prefences box.";
-  $result .= "If you make an incorrect selection, click on the candidate in the Preferences box, and it will move back to the Candidates box.  Note: you may need to scroll down to see the Preferences box.</p>";
-  $result .= "Once you have a Preferences box that you like, use the button at the bottom to continue to the next step.  You can review and confirm";
-  $result .= " your ballot on the next page.</p>";
-
   $result .= "<p>You can vote for as few or as many candidates as you choose. ";
   $result .= "An <a href=\"http://en.wikipedia.org/wiki/Single_transferable_vote\">STV algorithm</a> will be used to count your votes.</p>";
+
+  $result .= "<p>Clicking on a candidate in the <em>Candidates</em> box adds it in order to the Prefences box.  ";
+  $result .= "If you want to change a selection you've already made, you can click on the candidate in the <em>Preferences</em> box, and that candidate will move back to the <em>Candidates</em> box.  (Note: you may need to scroll down to see the <em>Preferences</em> box).</p>";
+  $result .= "<p>Once you have a <em>Preferences</em> box that you like, use the button at the bottom to continue to the next step.  You can review and confirm";
+  $result .= " your ballot on the next page.</p>";
 
   $result .= "<p><noscript>Note: This page requires Javascript</noscript></p>";
 
