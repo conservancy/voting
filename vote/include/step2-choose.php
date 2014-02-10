96683ddde9a0d272a2f3253baafb031d5588ecc9
Bradley M. Kuhn <bkuhn@ebb.org>
2014-02-10 17:14:18 -0500

    Clearer instructions for how to handle the voting.
    This was primarily based on feedback from Richard Fontana, who said the
    previous instructions were unclear.
    
    I also figured it's better to refer to third party documents on what STV is,
    rather than the vague description that was here previously.

--- vote/include/step2-choose.php
+++ vote/include/step2-choose.php
@@ -8,21 +8,15 @@ function step2_do () {
 
   $result = "<h2>Step 2/4 - Choose your vote</h2>\n";
 
-  $result .= "<p>Choose your candidates in the order of your preference by ";
-  $result .= "clicking on them. Don't worry, if you click on someone by mistake ";
-  $result .= "you can correct it later. Once you are happy with the order, ";
-  $result .= "submit your vote. You will have the chance to review and confirm";
+  $result .= "<p>Clicking on a candidate in the Candidates box adds it in order to the Prefences box.";
+  $result .= "If you make an incorrect selection, click on the candidate in the Preferences box, and it will move back to the Candidates box.  Note: you may need to scroll down to see the Preferences box.</p>";
+  $result .= "Once you have a Preferences box that you like, use the button at the bottom to continue to the next step.  You can review and confirm";
   $result .= " your ballot on the next page.</p>";
 
   $result .= "<p>You can vote for as few or as many candidates as you choose. ";
-  $result .= "Your vote will be counted for your first choice candidate as ";
-  $result .= "long as the candidate is still in the race, and when the candidate is eliminated, your ";
-  $result .= "vote will transfer to the next preference candidate still in the ";
-  $result .= "race.</p>";
+  $result .= "An <a href=\"http://en.wikipedia.org/wiki/Single_transferable_vote\">STV algorithm</a> will be used to count your votes.</p>";
 
   $result .= "<p><noscript>Note: This page requires Javascript</noscript></p>";
-
-  $result .= "<p>Possible choices:</p>\n";
 
   $result .= "<div class=\"canddata\">\n";
   $result .= "<h3>Candidates</h3>\n";
